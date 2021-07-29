195b44a8d453387ae420fc4c680b4c329b54dab8
Daniel Paul Tchatchouang Ouokam <tchatchouang.ouokam@gmail.com>
2021-07-29 23:46:55 +0200

    add checking step logic

--- src/app/components/navbar-checkout/navbar-checkout.component.ts
+++ src/app/components/navbar-checkout/navbar-checkout.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-checkout',
   templateUrl: './navbar-checkout.component.html',
-  styleUrls: ['./navbar-checkout.component.scss']
+  styleUrls: ['./navbar-checkout.component.scss'],
 })
 export class NavbarCheckoutComponent implements OnInit {
+  @Input() step: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
